e6591daa1d42f5bc5fed2c35ac73d0cedf996e46
drewnovok <drewnovok@gmail.com>
2022-05-03 08:51:02 +0300

    Add test for making backup file

--- tests/unitTests.cpp
+++ tests/unitTests.cpp
@@ -8,9 +8,12 @@
 INITIALIZE_EASYLOGGINGPP
 
 constexpr auto TASK_NAME = "task name";
-constexpr auto DATE_NAME = "22-04-2022";
+constexpr auto DATE_NAME = "22-4-2022";
 constexpr auto BACKUP_FILENAME = "test_data/backup_example.txt";
 constexpr auto CREATE_BACKUP_FILENAME = "my_backup.txt";
+
+constexpr auto TAB = "\t";
+constexpr auto NEW_LINE = "\n";
 
 TEST(ToDoListTests, AddTaskForTodayNormal)
 {
@@ -59,15 +62,49 @@ TEST(ToDoListTests, LoadBackupNormal)
 	{
 		auto date = Utils::BuildDateStr(elem);
 		ASSERT_TRUE(list.GetTasksAmountForDate(date) == taskCounter);
-		ASSERT_TRUE(list.GetTasksForDate(date) == std::vector<Task>(taskCounter++, { { "task name" } }));
+		ASSERT_TRUE(list.GetTasksForDate(date) == std::vector<Task>(taskCounter++, { { TASK_NAME } }));
 	}
 }
 
 TEST(ToDoListTests, MakeBackupNormal)
 {
 	ToDoList list;
+
+	for (auto i = 0; i < 10; i++)
+	{
+		list.AddTask(DATE_NAME, TASK_NAME);
+	}
+
 	list.MakeBackup(CREATE_BACKUP_FILENAME);
 	ASSERT_TRUE(std::filesystem::exists(CREATE_BACKUP_FILENAME));
+
+	std::ifstream backupFile(CREATE_BACKUP_FILENAME);
+	std::string line;
+	if (backupFile.is_open())
+	{
+		while (std::getline(backupFile, line))
+		{
+			ASSERT_TRUE(line.find(DATE_NAME + std::string(TAB) + TASK_NAME) != std::string::npos);
+		}
+	}
+}
+
+TEST(ToDoListTests, MakeBackupEmpty)
+{
+	ToDoList list;
+	list.LoadBackup(CREATE_BACKUP_FILENAME);
+
+	list.RemoveAllTasks();
+
+	list.MakeBackup(CREATE_BACKUP_FILENAME);
+	ASSERT_TRUE(std::filesystem::exists(CREATE_BACKUP_FILENAME));
+
+	std::ifstream backupFile(CREATE_BACKUP_FILENAME);
+	std::string line;
+	if (backupFile.is_open())
+	{
+		ASSERT_TRUE(backupFile.peek() == std::ifstream::traits_type::eof());
+	}
 }
 
 int main(int argc, char **argv)
